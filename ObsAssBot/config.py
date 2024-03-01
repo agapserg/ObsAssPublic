1cb2a6e28ee90cc5585728b0e9e3fba53fa0c5b8
agapserg <agapserg@gmail.com>
2024-03-01 23:23:29 +0500

    4-03-01-07 еще фиксы

--- ObsAssBot/config.py
+++ ObsAssBot/config.py
@@ -3,13 +3,13 @@ TOKEN = 'xxxxxxxxx'
 # Файл config.py
 
 # Определение корневого пути
-ROOT_PATH = "C:\\Git\\ObsAssPublic\\database\\"
+ROOT_PATH = "C:\\Git\\ObsAssPublic\\"
 
 # Определение путей, используя ROOT_PATH
-READ_LATER_PATH = ROOT_PATH + "readlater.md"
-BOOK_PATH = ROOT_PATH + "books\\"
-WAKE_UP_PATH = ROOT_PATH + "wakeup.md"
-SLEEP_PATH = ROOT_PATH + "sleep.md"
-VIDEO_PATH = ROOT_PATH + "video\\"
-PHOTO_PATH = ROOT_PATH + "photo\\"
+READ_LATER_PATH = ROOT_PATH + "database\\readlater.md"
+BOOK_PATH = ROOT_PATH + "database\\books\\"
+WAKE_UP_PATH = ROOT_PATH + "database\\wakeup.md"
+SLEEP_PATH = ROOT_PATH + "database\\sleep.md"
+VIDEO_PATH = ROOT_PATH + "database\\video\\"
+PHOTO_PATH = ROOT_PATH + "database\\photo\\"
 BD_PATH = ROOT_PATH + "ObsAssBot\\"
